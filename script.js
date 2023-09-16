101ecc69e6f77ddbe95c93fbb54b0a640d464720
Adam Farrow <adamfarrow1@gmail.com>
2023-09-16 06:20:05 -0400

    working api call

--- script.js
+++ script.js
@@ -1,16 +1,71 @@
 const button = document.getElementById("button");
 
+function getAllTextFromWebpageWithoutTags() {
+  let text = '';
+
+  function extractText(node) {
+    // Skip script and style elements
+    if (node.nodeName === 'SCRIPT' || node.nodeName === 'STYLE') {
+      return;
+    }
+
+    // If the node is a text node, append its content to the result
+    if (node.nodeType === Node.TEXT_NODE) {
+      text += node.textContent;
+    }
+
+    // Recursively process child nodes
+    if (node.childNodes) {
+      for (const childNode of node.childNodes) {
+        extractText(childNode);
+      }
+    }
+  }
+
+  // Start extracting text from the entire document body
+  extractText(document.body);
+
+  // Remove leading and trailing whitespace
+  text = text.trim();
+
+  return text;
+}
+
+
+
+
 button.addEventListener("click", async function() {
     // This function will be awaited before the next line of code is executed
 
     try {
+      
+      var paragraphTexts = document.documentElement.outerHTML;
+    //    const paragraphs = document.querySelectorAll('a');
+    // // Create an array to store the inner text of <p> tags
+    // const paragraphTexts = [];
+
+    // // Loop through the selected <p> tags and extract their inner text
+    //   paragraphs.forEach(paragraph => {
+    //     // Get the inner text of each <p> tag and push it to the array
+    //     const text = paragraph.innerText;
+    //     paragraphTexts.push(text);
+    //   });
+
+      console.log(paragraphTexts);
+
+
+
+
+
+
+
       const headers = new Headers();
       headers.append("Content-Type", "application/json");
-      headers.append("Authorization", `Bearer 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`);
+      headers.append("Authorization", `Bearer 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`);
       
       const requestBody = JSON.stringify({
         model: 'text-davinci-003',
-        prompt: "hello",
+        prompt: "summarize the text that is within this HTML file: " + paragraphTexts,
         max_tokens: 100
       });
 
